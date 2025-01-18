a436e7ca5beb539a8307dfa1eba60193eee0e925
Fahim Orko <fahimorko4122@gmail.com>
2025-01-18 18:22:48 +0200

    slight update

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,54 @@ const App = () => {
   const active = "active";
   // const step = 1;
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
   const handlePrev = () => {
-    step > 1 && setStep(step - 1);
+    return (
+      step > 1 &&
+      setStep((s) => {
+        // console.log(s - 1);
+        return s - 1;
+      })
+    );
   };
   const handleNext = () => {
-    step < 3 && setStep(step + 1);
+    return step < 3 && setStep((s) => s + 1);
   };
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? active : ""}>1</div>
-        <div className={step >= 2 ? active : ""}>2</div>
-        <div className={step >= 3 ? active : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step} : {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button style={buttonColor} onClick={handlePrev}>
-          Previous
-        </button>
-        <button style={buttonColor} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button
+        className="close"
+        onClick={() => {
+          // console.log("-------------------------------------");
+          // console.log("intaitally", isOpen);
+          // console.log("after change", !isOpen);
+          // console.log("-------------------------------------");
+          return setIsOpen((open) => !open);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? active : ""}>1</div>
+            <div className={step >= 2 ? active : ""}>2</div>
+            <div className={step >= 3 ? active : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button style={buttonColor} onClick={() => handlePrev()}>
+              Previous
+            </button>
+            <button style={buttonColor} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
