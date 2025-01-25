f1c9cc959316693fd65b91175fdcadf09bcfadf0
Fahim Orko <fahimorko4122@gmail.com>
2025-01-25 22:43:20 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,42 @@ import Navbar from "./Navbar";
 import { Main } from "./Main";
 import { Search } from "./Search";
 import { NavbarResults } from "./NavbarResults";
-import { MovieList } from "./MovieList";
-import { WatchedList } from "./WatchedList";
+import { Box } from "./Box";
 import { MovieInList } from "./MovieInList";
 import { tempWatchedData } from "./tempWatchedData";
 import { tempMovieData } from "./tempMovieData";
+import { WatchedMovieList } from "./WatchedMovieList";
+import { WatchedSumamry } from "./WatchedSummary";
+import { StarRaiting } from "./StarRaiting";
 
 export const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
   console.log(movies);
   return (
     <>
-      <Navbar>
+      {/* <Navbar>
         <Search />
         <NavbarResults movies={movies} />
       </Navbar>
       <Main>
-        <MovieList>
+        <Box>
           <MovieInList movies={movies} />
-        </MovieList>
-        <WatchedList />
-      </Main>
+        </Box>
+        <Box>
+          <WatchedSumamry watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </Box>
+      </Main> */}
+      <StarRaiting maxRating={5} />
+      <StarRaiting maxRating={5} defaultRating={3} />
+      <StarRaiting
+        maxRating={5}
+        masssages={["Terible", "Bad", "Okay", "Good", "Superb"]}
+      />
     </>
   );
 }
---
+++ src/StarRaiting.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Star } from "./Star";
+import PropTypes from "prop-types";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+};
+
+export const StarRaiting = ({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  defaultRating = 0,
+  masssages = [],
+}) => {
+  const [rating, setRating] = useState(defaultRating);
+  const [hoverRating, setHoverRating] = useState(0);
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color: color,
+    fontSize: `${size / 1.5}px`,
+  };
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span key={i - 1}>
+            <Star
+              key={i}
+              color={color}
+              size={size}
+              onRate={() => setRating(i + 1)}
+              full={hoverRating !== 0 ? hoverRating >= i + 1 : rating >= i + 1}
+              onHoverIn={() => setHoverRating(i + 1)}
+              onHoverLeave={() => setHoverRating(0)}
+            />
+          </span>
+        ))}
+      </div>
+      <p style={textStyle}>
+        {masssages.length === maxRating
+          ? masssages[hoverRating !== 0 ? hoverRating - 1 : rating - 1]
+          : hoverRating !== 0
+          ? hoverRating
+          : rating || ""}
+      </p>
+    </div>
+  );
+};
+
+StarRaiting.prototype = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  defaultRating: PropTypes.number,
+  masssages: PropTypes.array,
+};
